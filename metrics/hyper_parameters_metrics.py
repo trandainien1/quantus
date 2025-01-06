5ff3d7bf7d9e9af0774c50daebbe4e52110ad828
trandainien1@gmail.com <trandainien1@gmail.com>
2025-01-06 20:19:18 +0700

    update

--- metrics/hyper_parameters_metrics.py
+++ metrics/hyper_parameters_metrics.py
@@ -91,6 +91,9 @@ def get_hyper_param_eval(img_size=224, num_classes=1000):
         'Relevance Mass Ranking': {},
         'Attribution Localisation':{},
         'AUC':{},
+        'Pointing Game':{
+            
+        },
         #Randomisation
         'Model Parameter Randomisation':{
             'layer_order': 'top_down',
