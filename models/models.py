aa9bd84a6eb3c4a17e0c61aa811d7acead1fab5b
trandainien1@gmail.com <trandainien1@gmail.com>
2025-01-07 10:41:13 +0700

    update

--- models/models.py
+++ models/models.py
@@ -9,16 +9,12 @@ def get_model(name, n_output, dataset=None, checkpoint=None, pretrained=True, me
         # model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
         # model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True)
         if 'agc' in method_name:
-            timm_model = timm.create_model(MODEL, pretrained=pretrained, num_classes=n_output, pretrained_cfg=pretrained_cfg)
+            # timm_model = timm.create_model(MODEL, pretrained=pretrained, num_classes=n_output, pretrained_cfg=pretrained_cfg)
+            timm_model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
             state_dict = timm_model.state_dict()
             model = ViT_Ours.create_model(MODEL, pretrained=pretrained, num_classes=n_output)
             model.load_state_dict(state_dict, strict=True)
-        else:
-            # model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True)
-            model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg=pretrained_cfg)
-
-    print('------- [MODEL Default config] -------')
-    print(model.default_cfg)
-    print()
- 
-    return model
+        else: 
+            model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
+    
+        return model
