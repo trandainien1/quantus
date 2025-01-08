3891b368f758dd54fd1d1036699d9febb919fb4f
trandainien1 <trandainien1@gmail.com>
2025-01-08 14:01:25 +0700

    'update'

--- models/models.py
+++ models/models.py
@@ -6,15 +6,12 @@ MODEL = 'vit_base_patch16_224'
 def get_model(name, n_output, dataset=None, checkpoint=None, pretrained=True, method_name='', pretrained_cfg=None):
     
     if name == 'vit_b16':
-        # model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg='orig_in21k_ft_in1k')
-        # model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True)
         if 'agc' in method_name:
             timm_model = timm.create_model(MODEL, pretrained=pretrained, num_classes=n_output, pretrained_cfg=pretrained_cfg)
             state_dict = timm_model.state_dict()
             model = ViT_Ours.create_model(MODEL, pretrained=pretrained, num_classes=n_output)
             model.load_state_dict(state_dict, strict=True)
         else:
-            # model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True)
             model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg=pretrained_cfg)
 
     print('------- [MODEL Default config] -------')
