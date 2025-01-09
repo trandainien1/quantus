434e6e8e40f8e3b333db06c89ebb09cc00a342bd
trandainien1 <trandainien1@gmail.com>
2025-01-09 14:33:22 +0700

    'update'

--- tis/tis.py
+++ tis/tis.py
@@ -289,7 +289,7 @@ class TIS:
                 exit(1)
 
         # Compute scores by batch
-        for idx in tqdm(range(math.ceil(len(mask_indices_list) / self.batch_size)), disable=(not self.verbose)):
+        for idx in tqdm(range(math.ceil(len(mask_indices_list) / self.batch_size)), disable=(self.verbose):
             # Select the masks attributed to the current batch
             selection_slice = slice(idx * self.batch_size, min((idx + 1) * self.batch_size, len(mask_indices_list)))
             self.cur_mask_indices = mask_indices_list[selection_slice]
