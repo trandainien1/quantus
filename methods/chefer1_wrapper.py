246561e79ac5c2e87001a729429705d8b0d5a55c
trandainien1@gmail.com <trandainien1@gmail.com>
2025-01-23 18:12:29 +0700

    update

--- methods/chefer1_wrapper.py
+++ methods/chefer1_wrapper.py
@@ -1,13 +1,13 @@
 import torch
 
-try:
-    from chefer.baselines.ViT.ViT_explanation_generator import LRP
-    from chefer.baselines.ViT.ViT_LRP import VisionTransformer, _conv_filter, _cfg
-    from chefer.baselines.ViT.helpers import load_pretrained
-    from timm.models.vision_transformer import default_cfgs as vit_cfgs
-    print('SUCCESS: chefer was successfully imported.')
-except:
-    print('ERROR: chefer was not found.')
+# try:
+from chefer.baselines.ViT.ViT_explanation_generator import LRP
+from chefer.baselines.ViT.ViT_LRP import VisionTransformer, _conv_filter, _cfg
+from chefer.baselines.ViT.helpers import load_pretrained
+from timm.models.vision_transformer import default_cfgs as vit_cfgs
+# print('SUCCESS: chefer was successfully imported.')
+# except:
+# print('ERROR: chefer was not found.')
 
 def vit_base_patch16_224(pretrained=True, model_name="vit_base_patch16_224", pretrained_cfg='orig_in21k_ft_in1k', **kwargs):
     model = VisionTransformer(patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True, **kwargs)
