75535c3b64c4cc53a0d0d30e30b20042d4678194
trandainien1 <trandainien1@gmail.com>
2025-02-07 10:50:29 +0700

    update

--- models/models.py
+++ models/models.py
@@ -2,16 +2,23 @@ import timm
 import AGC_methods.AGCAM.ViT_for_AGCAM as ViT_Ours
 import torch.utils.model_zoo as model_zoo
 MODEL = 'vit_base_patch16_224'
+from tam.baselines.ViT.ViT_LRP import vit_base_patch16_224 as LRP_vit_base_patch16_224
 
 def get_model(name, n_output, dataset=None, checkpoint=None, pretrained=True, method_name='', pretrained_cfg=None):
     
     if name == 'vit_b16':
-        if 'agc' in method_name or 'rollout' or 'tam' == method_name:
+        if 'agc' in method_name or 'rollout' == method_name:
             # timm_model = timm.create_model(MODEL, pretrained=pretrained, num_classes=n_output, pretrained_cfg=pretrained_cfg)
             timm_model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg=pretrained_cfg)
             state_dict = timm_model.state_dict()
             model = ViT_Ours.create_model(MODEL, pretrained=pretrained, num_classes=n_output)
             model.load_state_dict(state_dict, strict=True)
+        elif method_name == 'tam':
+            model = LRP_vit_base_patch16_224('cuda', num_classes=1000).to('cuda')
+            timm_model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg=pretrained_cfg)
+            state_dict = timm_model.state_dict()
+            model.load_state_dict(state_dict, strict=True)
+            model.eval()
         else:
             model = timm.create_model(model_name='vit_base_patch16_224', pretrained=True, pretrained_cfg=pretrained_cfg)
 
